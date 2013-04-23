748393865aecb919431b552a44b3ac8f6b1e394c
Derrick Pelletier <derrick@dpelletier.com>
2013-04-23 11:00:07 -0700

    Changed to reflect new options

--- example.js
+++ example.js
@@ -3,12 +3,18 @@ var status = require('./status.js')
 
 status.addItem("Total", {
   max:100, 
-  percentage:true,
+  type:"percentage",
   color:'cyan'
 })
 
 status.addItem("ERR", {
   color:'red'
+})
+
+status.addItem("Pizza", {
+  color:'green',
+  type:['bar','percentage'],
+  max:50
 })
 
 console.log("Starting timer")
@@ -18,8 +24,9 @@ var runner = function() {
 	times++
   status.updateItem('Total', 5)
   status.updateItem('ERR', 1)
+  status.updateItem('Pizza', 1)
 	if(times == 10) {
-		status.stamp()
+		// status.stamp()
 		times = 0
 	}
   setTimeout(runner, it)
