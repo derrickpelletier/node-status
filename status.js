1ede6a765b01a78ec25ebce6dc6119d66d8f5208
Derrick Pelletier <derrick@dpelletier.com>
2013-04-23 10:59:56 -0700

    Added statusbar types. Changed how types are handled. Added ability to use multiple types in one item

--- status.js
+++ status.js
@@ -1,13 +1,14 @@
 var colors = require('colors')
 var start = new Date().getTime()
 var pad = "  "
+
 var Item = function(options) {
 	this.label = options.label
 	this.count = 0
 	options.count && (this.count = options.count)
 	options.max && (this.max = options.max)
-	options.percentage &&	(this.percentage = true)
 	options.color && (this.color = colors[options.color])
+	this.type = (options.type) ? options.type : "count"
 }
 
 var items = {}
@@ -17,20 +18,55 @@ var render = function(stamp){
   for (var i in items) {
     var c = items[i]
     var text = c.color ? c.color(c.label) : c.label
-    var nums = c.count
-    if(c.max && c.percentage) {
-    	nums = (100 * c.count/c.max).toFixed(3) + " %"
-    } else if (c.max) {
-    	nums += "/" + c.max
-    }
+
+
+
+    var nums = ""
+
+    var types = c.type
+    if( Object.prototype.toString.call( types ) !== '[object Array]' ) {
+    	types = [c.type]
+		}
+
+		for (var a = 0; a < types.length; a++) {
+			if(a > 0)
+				nums += pad
+	    switch(types[a]) {
+	    	case "percentage":
+	    		nums += (100 * c.count/c.max).toFixed(3) + " %"
+	    		break;
+	    	case "bar":
+	    		var done = Math.round(10 * c.count/c.max) 
+	    		nums += "["
+	    		for (var i = 0; i < done; i++) {
+	    			nums += "▒".white
+	    		}
+	    		for (var i = 0; i < 10-done; i++) {
+	    			nums += "-".black
+	    		}
+	    		nums += "]"
+	    		break;
+	    	default:
+	    		nums += c.count
+	    		if(c.max)
+	    			nums += "/" + c.max
+	    		break;
+	    }
+	  }
     out += "|" + pad + text + ": " + nums + pad
   }
   out +=  " |"
   if(stamp) {
   	process.stdout.write("\u001B[2K")
-  	console.log((new Date().getTime() - start) + "ms " + out + "\r")
+  	console.log("@ " + nicetime(new Date().getTime() - start) + out + "\r")
   }
-  process.stdout.write("  Status: " + out + "\r")
+  process.stdout.write("\u001B[2KStatus: " + out + "\r")
+}
+
+var nicetime = function(ms){
+	var seconds = ms / 1000
+	var minutes = seconds / 60
+	return (minutes < 2) ? seconds + "s " : minutes + " mins "
 }
 
 exports.addItem = function(label, options){
