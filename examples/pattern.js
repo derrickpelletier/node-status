ea81e89957340d1d9c294862c6c18519ec19768a
Derrick Pelletier <derrick@dpelletier.com>
2016-08-23 13:21:01 -0700

    updating standard example

--- examples/pattern.js
+++ examples/pattern.js
@@ -1,15 +1,13 @@
-//
-var status = require('../status.js')
-  , os = require('os')
-  , console = status.console()
+const status = require('../status.js');
+const console = status.console();
 
 var total = status.addItem('total', {
   max: 100
-})
+});
 
 var err_count = status.addItem('ERR', {
   label: 'errors'
-})
+});
 
 var pizza = status.addItem('pizza', {
   label: 'pizza eaten',
@@ -19,33 +17,29 @@ var pizza = status.addItem('pizza', {
   custom: function () {
     return `${this.count} slices`;
   }
-})
+});
 
-
-status.pattern = '{timestamp} | {total.spinner} {total.percentage}';
-
-console.log('Starting timer for status bar example...\n')
-
-var it = 500
-var times = 0
-var runner = function() {
-	times++
-  if (times % 3 === 0)
-    total.inc(5)
-  err_count.inc()
-  if (times % 2 === 0)
-    pizza.inc()
-	if(times % 10 === 0) {
-		// console.log('Logging something arbirtrary', total.count, err_count.count)
-	}
-  if(times < 25) {
-    setTimeout(runner, it)
-  } else {
-    status.stop()
-  }
-}
+console.log('Starting timer for status bar example...\n');
 
 status.start({
   pattern: '{uptime.green} {spinner.cyan}  |  Total: {total.percentage}  |  Pizza eaten: {pizza.green.bar} {pizza.custom.magenta}'
-})
-runner()
+});
+
+var it = 500;
+var times = 0;
+var runner = function () {
+	times++;
+  if (times % 3 === 0) total.inc(5);
+  err_count.inc();
+  if (times % 2 === 0) pizza.inc();
+	if(times % 10 === 0) {
+		console.log('Logging something arbirtrary', total.count, err_count.count)
+	}
+  if(times < 25) {
+    setTimeout(runner, it);
+  } else {
+    status.stop();
+  }
+}
+
+runner();
